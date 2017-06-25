b6ffa58014b88164c583e6b8ca5a95a3191ff8a1
P S @ Art <singh.prabesh@hotmail.com>
2017-06-25 15:37:44 -0700

    select with condition updated

--- www/test/select_box/select_withCondition.php
+++ www/test/select_box/select_withCondition.php
@@ -1,16 +1,31 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/PS_settings/PS_master.php');
+?>
 
 
+<?php
 $arrCond = [
     'id' => ['>' => '1'],
     'source' => ['=' => 'bbc-sport'],
-
 ];
 
- $a =  PS_cl_static::make_dropDown_arrCond('main_news', 'id', 'title', '1', $arrCond, '1');
+$a = PS_cl_static::make_dropDown_arrCond('main_news', 'id', 'title', '1', $arrCond, '1');
 ?>
-
 <select name="" id="">
-    <option value=""> Please select.. </option>
- <?php echo $a ?>
+    <option value=""> Please select..</option>
+
+    <?php echo $a ?>
+</select>
+<HR>
+<?php
+$arrCond = [
+//    'id' => ['>' => '1'],
+//    'source' => ['=' => 'bbc-sport'],
+];
+
+$b = PS_cl_static::make_dropDown_arrCond('main_news', 'id', 'title', '1', '', '1');
+?>
+<select name="" id="">
+    <option value=""> Please select..</option>
+
+    <?php echo $b ?>
 </select>
